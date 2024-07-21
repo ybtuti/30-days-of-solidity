ba1d59f7f47963577ff2c1d0b00e19b295a88b1b
ybtuti <samuelmsuu@gmail.com>
2024-07-21 14:09:25 +0300

    chore: Add necessary files for HoroscopeNFT frontend deployment and update dependencies

---
+++ horoscopeweb3/src/App.js
@@ -0,0 +1,166 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from "react";
+import { Contract, providers } from "ethers";
+
+const [date, setDate] = useState("1992-08-31");
+const [zodiacSign, setZodiacSign] = useState(null);
+
+function App() {
+  const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+
+  // state for whether the app is minting or not.
+  const [isMinting, setIsMinting] = useState(false);
+
+  const [NFTContract, setNFTContract] = useState(null);
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      setIsWalletInstalled(true);
+    }
+  }, []);
+
+  function handleDateInput({ target }) {
+    setDate(target.value);
+  }
+
+  async function connectWallet() {
+    window.ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .then((accounts) => {
+        setAccount(accounts[0]);
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+      });
+  }
+
+  useEffect(() => {
+    calculateZodiacSign(date);
+  }, [date]);
+
+  function hadleDateInput({ target}) {
+    setDate(target.value);
+  }
+
+  function calculateZodiacSign(date) {
+    let dateObject = new Date(date);
+    let day = dateObject.getDate();
+    let month = dateObject.getMonth();
+    if (month == 0) {
+      if (day >= 20) {
+        setZodiacSign("Aquarius");
+      } else {
+        setZodiacSign("Capricorn");
+      }
+    } else if (month == 1) {
+      if (day >= 19) {
+        setZodiacSign("Pisces");
+      } else {
+        setZodiacSign("Aquarius");
+      }
+    } else if (month == 2) {
+      if (day >= 21) {
+        setZodiacSign("Aries");
+      } else {
+        setZodiacSign("Pisces");
+      }
+    } else if (month == 3) {
+      if (day >= 20) {
+        setZodiacSign("Taurus");
+      } else {
+        setZodiacSign("Aries");
+      }
+    } else if (month == 4) {
+      if (day >= 21) {
+        setZodiacSign("Gemini");
+      } else {
+        setZodiacSign("Taurus");
+      }
+    } else if (month == 5) {
+      if (day >= 21) {
+        setZodiacSign("Cancer");
+      } else {
+        setZodiacSign("Gemini");
+      }
+    } else if (month == 6) {
+      if (day >= 23) {
+        setZodiacSign("Leo");
+      } else {
+        setZodiacSign("Cancer");
+      }
+    } else if (month == 7) {
+      if (day >= 23) {
+        setZodiacSign("Virgo");
+      } else {
+        setZodiacSign("Leo");
+      }
+    } else if (month == 8) {
+      if (day >= 23) {
+        setZodiacSign("Libra");
+      } else {
+        setZodiacSign("Virgo");
+      }
+    } else if (month == 9) {
+      if (day >= 23) {
+        setZodiacSign("Scorpio");
+      } else {
+        setZodiacSign("Libra");
+      }
+    } else if (month == 10) {
+      if (day >= 22) {
+        setZodiacSign("Sagittarius");
+      } else {
+        setZodiacSign("Scorpio");
+      }
+    } else if (month == 11) {
+      if (day >= 22) {
+        setZodiacSign("Capricorn");
+      } else {
+        setZodiacSign("Sagittarius");
+      }
+    }
+  }
+  return (
+    <div className="App">
+      <h1>Horoscope NFT Minting Dapp</h1>
+      <p> Connected as: {account}</p>
+
+      <input onChange={handleDateInput} value={date} type="date" id="dob" />
+      <br />
+      <br />
+      {zodiacSign ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          preerveAspectRatio="xMidYMid meet"
+          viewBox="0 0 300 300"
+          width="400px"
+          height="400px"
+        >
+          <style>{`.base { fill: white; font-family: serif; font-size: 24px;`}</style>
+          <rect width="100%" height="100%" fill="black" />
+          <text 
+            x="50%" 
+            y="50%" 
+            class="base"
+            dorminant-baseline="middle"
+            text-anchor="middle"
+          >
+            {zodiacSign}
+          </text>
+        </svg>
+      ) : null}
+
+      <br />
+      <br />
+      <button disables={isMinting} onClick={mintNFT}>
+        {isMinting ? "Minting..." : "Mint NFT"}
+      </button>
+      </div>
+  );
+}
+export default App;
